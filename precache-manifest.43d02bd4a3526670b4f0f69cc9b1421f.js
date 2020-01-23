b919a6978ad5d29f29e5bcad45b4d2efa1d91ab5
Mariano Conti <nanexcool@gmail.com>
2020-01-22 22:15:19 -0300

    Updates

---
+++ precache-manifest.43d02bd4a3526670b4f0f69cc9b1421f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f22290fe3208d5f685ee276deeb8b9f",
+    "url": "/makerhero/index.html"
+  },
+  {
+    "revision": "2e8a564279891846a23d",
+    "url": "/makerhero/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9f5f0f12555e8f9c8013",
+    "url": "/makerhero/static/js/2.cbf736e9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/makerhero/static/js/2.cbf736e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e8a564279891846a23d",
+    "url": "/makerhero/static/js/main.8f13f41f.chunk.js"
+  },
+  {
+    "revision": "d551be8d31521591d401",
+    "url": "/makerhero/static/js/runtime-main.2b0e7e6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/makerhero/static/media/logo.5d5d9eef.svg"
+  }
+]);
